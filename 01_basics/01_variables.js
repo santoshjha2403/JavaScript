cf1c2768fbc60ed8a31f8c81e2c0c27b94f07e6c
santoshjha2403 <sk081jha@gmail.com>
2025-02-16 20:05:19 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 122344;
+let accountEmail = 'san@123';
+var accountPass = '12345';
+accountCity = 'bihar'
+
+console.table([accountId,accountEmail,accountPass,accountCity]);
+ //prefere not to use var because of block scope issue
+
+ //ec39.es is the main website to lear js
+
+ 
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat  all js code as newer version.
+
+// alert('hello'); // we are using node not browser
+
+//number = 234
+
+//string = 'test'
+
+//boolean = true false
+
+// null
+
+//undefined
+
+console.log(typeof undefined);
+
+console.log(typeof null);
+
+// do check the documentation on ecma script(es)
