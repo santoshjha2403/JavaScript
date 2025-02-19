528fabad54b9ac401e2737b55aea300eb0a6d643
santoshjha2403 <sk081jha@gmail.com>
2025-02-19 13:32:06 +0000

    till 6th is completed

---
+++ 01_basics/05_sting.js
@@ -0,0 +1,5 @@
+const gameName = new String('hello')
+const playerName = 'ram'
+
+console.log(gameName[0]);
+console.log(playerName[0]);
---
+++ 01_basics/06_numsAndMaths.js
@@ -0,0 +1,23 @@
+const num = 400;
+
+console.log(num); // response '400'
+
+const score = new Number(400)
+
+console.log(score);// response 'Number 400'
+
+console.log(score.toFixed(2)); // response '400.00'
+
+
+//*************maths***********************
+console.log(Math);
+console.log(Math.abs(-4));// response ' 4'
+console.log(Math.round(4.6)); //response '5'
+console.log(Math.round(4.3));//response '4'
+console.log(Math.ceil(4.3)); //response '5'
+console.log(Math.floor(4.9));//response '4'
+
+
+
+
+
