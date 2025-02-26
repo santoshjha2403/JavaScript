4f7f8425d9c1dd5491c4e4f7723ea93be9eb1894
santoshjha2403 <sk081jha@gmail.com>
2025-02-26 15:35:55 +0000

    com

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,11 @@
+const myDate = new Date();
+
+console.log(myDate.toDateString());
+
+console.log(myDate.toISOString);
+
+console.log(myDate.toJSON());
+
+console.log(myDate.toLocaleDateString());
+
+console.log(myDate.toLocaleString());
---
+++ 02_basics/01_array.js
@@ -0,0 +1,5 @@
+const data = [1,2,3,4,5,6]
+const data1 = new Array('sant','mant')
+
+console.log(data)
+console.log(data1)
